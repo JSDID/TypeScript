c4ba7fb0dd1dce889805b74d91c02d8db0c1d904
JSDID <icegouse@gmail.com>
2023-11-14 13:06:36 +0200

    Refactoring structure

---
+++ src/01-primitives.js
@@ -0,0 +1,28 @@
+"use strict";
+// numbers
+let x = 10.5;
+// let y = 12345n;
+let z = NaN;
+// string
+let str1;
+str1 = 'Hello world';
+let symb = Symbol('as');
+// let d = str1 + symb
+// boolean
+let o = true;
+o = false;
+// o = 1
+// nothing
+let h = undefined;
+let g = null;
+// literal
+const num = 108;
+const str2 = 'str';
+// universal
+let mm = 1;
+mm = 'str';
+mm = [];
+mm.toUpperCase();
+let xx = 2;
+if (typeof xx === "string")
+    xx.toUpperCase();
