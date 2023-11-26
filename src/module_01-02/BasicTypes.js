d581e5485cd62cef4ac4b668015bb188c3e6a23d
JSDID <icegouse@gmail.com>
2023-11-26 12:34:59 +0200

    Массивы

--- src/module_01-02/BasicTypes.js
+++ src/module_01-02/BasicTypes.js
@@ -3,3 +3,7 @@ let x = 20;
 let y = "script";
 console.log(x);
 console.log(y);
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+const string = ["s", "c", "r", "i", "p", "t"];
+console.log(string);
--- src/module_01-02/BasicTypes.ts
+++ src/module_01-02/BasicTypes.ts
@@ -5,3 +5,8 @@ let y: string = "script";
 console.log(x);
 console.log(y);
 
+const numbers: Array<number> = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+const string: Array<string> = ["s", "c", "r", "i", "p", "t"];
+console.log(string);
