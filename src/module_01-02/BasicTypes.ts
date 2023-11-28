9cbf467877d5b7eb6ce82dfb0a87905f94d77f02
JSDID <icegouse@gmail.com>
2023-11-28 15:11:31 +0200

    Add carthage

--- src/module_01-02/BasicTypes.ts
+++ src/module_01-02/BasicTypes.ts
@@ -119,3 +119,8 @@ function printName(user: {
 
 console.log(printName({ firstName: 'Mikhail', lastName: 'Broun' }))
 
+// Картежы 
+const pars: [string, string][] = [['key1', 'value1'], ['key2', 'value2']];
+const data: [number, boolean, string] = [1, true, 'lodash'];
+const doc: [string, string, number, Date][] = [];
+doc.push(['Alex', 'Neb', 12, new Date(1992, 22, 8)]);
