6adf36a1a7b4ceb368628aa35b86bc9ed5593e2b
JSDID <icegouse@gmail.com>
2023-11-30 13:25:21 +0200

    Add Aliace

--- src/modul_02/types.js
+++ src/modul_02/types.js
@@ -1,3 +1,25 @@
 "use strict";
 window.isAuth;
-// class MyDeveloper implements Developer {}
+// const example: Union5 = {}
+// type User = {
+//   readonly email: string,
+//   readonly login: string,
+//   password: string,
+// }
+// type Person = {
+//   readonly firstName: string,
+//   lastName: string,
+//   phone?: string,
+//   yearOfBirth?: number,
+// }
+// type Employee = {
+//   contractStart: Date,
+// } & User & Person
+// // const user1: Employee = {}
+// type Developer = {
+//   skills: string[],
+//   phone: string,
+//   level?: 'junior' | 'middle' | 'senior',
+//   say(): void,
+//   code?: (arg: string) => void,
+// } & Employee
