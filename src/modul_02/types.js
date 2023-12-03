7fc82f74bc5050735c710e851bf130ec73af0b16
JSDID <icegouse@gmail.com>
2023-12-03 22:03:45 +0200

    Add narrowing

--- src/modul_02/types.js
+++ src/modul_02/types.js
@@ -1,10 +1,6 @@
 "use strict";
 console.clear();
 console.log('modul_2');
-// type Employee = {
-//   contractStart: Date,
-// } & User & Person
-// // const user1: Employee = {}
 // type Developer = {
 //   skills: string[],
 //   phone: string,
@@ -16,5 +12,44 @@ console.log('modul_2');
 // 1. Не можем второй раз создать глобально
 // 2. Можно создавать Интерфейс
 // Интерфейс
-// 1. Когда хотим расширать нужен Интерфейс
-// 2. Классах используеться implements 
+// 1. Когда хотим расширать нужен Интерфейс (extends)
+// 2. Классах используеться (implements) 
+// Сужение типов (narrowing)
+function example1(x) {
+    if (typeof x === "string") {
+        x.toLowerCase();
+    }
+    else if (typeof x === "number") {
+        x.toFixed();
+    }
+    else if (x === undefined) {
+        console.log('no value');
+    }
+    else {
+        x;
+    }
+}
+function example2(strs) {
+    // if (typeof strs === "object") {}
+    // if (Array.isArray(strs)) {
+    if (strs && typeof strs === "object") {
+        strs.concat([]);
+    }
+    else if (typeof strs === "string") {
+        strs.toLowerCase();
+    }
+}
+function example3(x) {
+    if (x instanceof Date) {
+        x.getMonth();
+    }
+    else {
+        x.concat([]);
+    }
+}
+function move(animal) {
+    if ('swim' in animal) {
+        return animal.swim();
+    }
+    return animal.fly();
+}
--- src/modul_02/types.ts
+++ src/modul_02/types.ts
@@ -59,6 +59,53 @@ type Union5 = { a: string, b: string, c: number } & { a: string, t: boolean, z: 
 // В чем разница между Интерфейсами и Алясами 
 // 1. Не можем второй раз создать глобально
 // 2. Можно создавать Интерфейс
+
 // Интерфейс
 // 1. Когда хотим расширать нужен Интерфейс (extends)
 // 2. Классах используеться (implements) 
+
+// Сужение типов (narrowing)
+function example1(x?: number | string) {
+    if (typeof x === "string") {
+        x.toLowerCase()
+    } else if (typeof x === "number") {
+        x.toFixed()
+    } else if (x === undefined) {
+        console.log('no value')
+    } else {
+        x
+    }
+}
+
+
+function example2(strs: string | string[] | null) {
+    // if (typeof strs === "object") {}
+    // if (Array.isArray(strs)) {
+    if (strs && typeof strs === "object") {
+        strs.concat([])
+    } else if (typeof strs === "string") {
+        strs.toLowerCase()
+    }
+}
+
+
+function example3(x: number[] | Date) {
+    if (x instanceof Date) {
+        x.getMonth();
+    } else {
+        x.concat([]);
+    }
+}
+
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+
+function move(animal: Fish | Bird) {
+    if ('swim' in animal) {
+        return animal.swim()
+    }
+
+    return animal.fly();
+}
+
+
